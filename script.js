8635f09954c12198cbcea50b325f8566704d975b
mohamed <man607123@gmail.com>
2025-03-11 22:53:52 +0200

    Added frontend and backend in one project

---
+++ script.js
@@ -0,0 +1,15 @@
+const apiUrl = "https://embeddedproject-production.up.railway.app/get-data";
+
+async function fetchData() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        document.getElementById("data").textContent = data.distance;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        document.getElementById("data").textContent = "Error!";
+    }
+}
+
+setInterval(fetchData, 1000);
+fetchData();
