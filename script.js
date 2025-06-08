894b13e0915b839855977d08efdef2df1fdbfa09
Mo-Nagi <121899295+Mo-Nagi@users.noreply.github.com>
2025-06-08 18:18:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://embeddedproject-production.up.railway.app";
+const apiUrl = "https://embeddedsysproject.netlify.app";
 
 let depthChart;
 const ctx = document.getElementById("depthChart").getContext("2d");
